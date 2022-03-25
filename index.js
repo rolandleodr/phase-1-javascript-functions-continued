fa94165fda1637d9bfc5adf075d9e41e556ea188
Roland Leo Del Rosario <rleoadr@gmail.com>
2022-03-25 00:36:09 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(string = 'roller-skate') {
+    return `This Saturday, I want to ${string}!`
+}
+function mondayWork(string = 'go to the office') {
+    return `This Monday, I will ${string}.`
+}
+function wrapAdjective(action = "*") {
+    const innerFunction = function (name = "special") {
+        return `You are ${action}${name}${action}!`;
+    }
+    return innerFunction;
+}
